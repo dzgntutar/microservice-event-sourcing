ca2a9ebbc80c85368dc0b34c7f60da947c6d57d8
dzgntutar <duzguntutar@gmail.com>
2022-05-27 20:37:29 +0300

    Events

---
+++ EventSourcing/EventSourcing.Shared/Events/BookCreatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcing.Shared.Events
+{
+    public class BookCreatedEvent :IEvent
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Author { get; set; } = default!;
+        public string Year { get; set; } = default!;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ EventSourcing/EventSourcing.Shared/Events/BookPriceChangedEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSourcing.Shared.Events
+{
+    public class BookPriceChangedEvent : IEvent
+    {
+        public Guid Id { get; set; }
+        public decimal NewPrice{ get; set; }
+    }
+}
