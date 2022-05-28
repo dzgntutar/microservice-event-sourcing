0c59f2c5abba472e13605f6e6543f75c8e5a6505
dzgntutar <duzguntutar@gmail.com>
2022-05-28 14:26:10 +0300

    Stream and di integraiton

---
+++ EventSourcing/EventSourcing.Api/EventStores/AbstractStream.cs
@@ -0,0 +1,39 @@
+﻿using EventSourcing.Shared.Events;
+using EventStore.ClientAPI;
+using System.Text;
+using System.Text.Json;
+
+namespace EventSourcing.Api.EventStores
+{
+    public abstract class AbstractStream
+    {
+        protected readonly LinkedList<IEvent> Events = new LinkedList<IEvent>();
+
+        private string _streamName { get; }
+
+        private readonly IEventStoreConnection _eventStoreConnection;
+
+        protected AbstractStream(string streamName, IEventStoreConnection eventStoreConnection)
+        {
+            _streamName = streamName;
+            _eventStoreConnection = eventStoreConnection;
+        }
+
+
+        public async Task SaveAsync()
+        {
+            var newEvents = Events.Select(_ =>
+                                                new EventData(
+                                                    Guid.NewGuid(),
+                                                    _.GetType().Name,
+                                                    true,
+                                                    Encoding.UTF8.GetBytes(JsonSerializer.Serialize(_, inputType: _.GetType())),
+                                                    Encoding.UTF8.GetBytes(_.GetType().FullName))
+                                          ).ToList();
+            await _eventStoreConnection.AppendToStreamAsync(_streamName, ExpectedVersion.Any, newEvents);
+
+            Events.Clear();
+        }
+
+    }
+}
